c544cbd29779f47260ff39a742d61983f577e2e9
Anzori-PR <saxelashvilianzori@gmail.com>
2023-05-01 22:42:13 +0400

    name change

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./dist/*.{html,js}"],
+    content: ["./workSpace/*.{html,js}"],
     theme: {
         container: {
             center: true,
